d4cb7f56b197985b0874bf2297d3dec55108ddd4
bodasooqa <timlol9894@gmail.com>
2022-12-27 14:53:31 +0800

    main [+] Fix cors origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin:
-        process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '*',
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:3000'
+          : 'http://localhost:3000',
     },
   });
   await app.listen(8000);
